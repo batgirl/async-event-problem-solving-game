7507a6a5fd4f582ca5455292f99b3c2c10809a55
Chloe Alnaji <caedeal@gmail.com>
2015-08-14 12:28:06 -0600

    second story commit

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 (function() { 
+
   var userStatus = {
     energy: 100,
     steps: 0
   };
-  
-  var status = document.getElementById("status");
-  for (key in userStatus) {
-    if (userStatus[key] > 0) {
-    status.innerHTML = status.innerHTML + "<div>" + key + ": " + userStatus[key] + "</div>"; 
-  }
-}})();
 
-var button = document.getElementById("walk");
-function disableButton() {
-  button.setAttribute("disabled", "true");
-  function enableButton() {
-    button.removeAttribute("disabled");
+  var status = document.getElementById("status");
+
+  var button = document.getElementById("walk");
+
+  var left = document.getElementById("left");
+
+  function disableButton() {
+    button.setAttribute("disabled", "true");
+    function enableButton() {
+      button.removeAttribute("disabled");
+    };
+    setTimeout(enableButton, 3000);
+    userStatus.steps++;
+    left.innerHTML = left.innerHTML + "<div>you continue down the trail</div>";
+    updateDom();
+  };
+
+  button.onclick = disableButton;
+
+  function updateDom() {
+    status.innerHTML = "";
+    for (key in userStatus) {
+      if (userStatus[key] > 0) {
+        status.innerHTML = status.innerHTML + "<div>" + key + ": " + userStatus[key] + "</div>"; 
+      }
+    }
   }
-  setTimeout(enableButton, 3000);
-};
-button.onclick = disableButton;
+
+updateDom();
+
+})();
 
